c5d147b6a965269ce496d84a9e8b2b1c90e914ff
Ahmed Shaaban <111895280+AhmedShaban52@users.noreply.github.com>
2025-10-17 17:23:36 +0300

    11

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,32 +1,24 @@
+// src/server.js
 import express from "express";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
-
 import { connectDB } from "./lib/db.js";
+
+connectDB();
 
 const app = express();
 
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
-    credentials: true, // allow frontend to send cookies
+    credentials: true,
   })
 );
-
-const PORT = 5001 || process.env.PORT || 5001;
-
-app.get("/", (request, response) => {
-  ///server to client
-  response.json({
-    message: "Server is running " + PORT,
-  });
-});
 
 app.use(express.json());
 app.use(cookieParser());
@@ -35,7 +27,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-  connectDB();
+app.get("/", (req, res) => {
+  res.json({ message: "Server is running on Vercel" });
 });
+
+export default app;
