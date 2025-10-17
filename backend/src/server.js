ab634599eadfc4b95a03aabe079ce4e5bfd84c32
Ahmed Shaaban <111895280+AhmedShaban52@users.noreply.github.com>
2025-10-17 14:53:36 +0300

    change

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,13 +11,11 @@ import chatRoutes from "./routes/chat.route.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
-const PORT = process.env.PORT;
-
-const __dirname = path.resolve();
+const PORT = 5001 || process.env.PORT;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true, // allow frontend to send cookies
   })
 );
@@ -28,14 +26,6 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
