82414d2d0466dd5ef8f0786cbf90b32ec302a4d4
Ahmed Shaaban <111895280+AhmedShaban52@users.noreply.github.com>
2025-10-17 18:13:40 +0300

    11

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,24 @@
-// src/server.js
 import express from "express";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
+
 import { connectDB } from "./lib/db.js";
 
-connectDB();
-
 const app = express();
+const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
+    origin: "http://localhost:5173",
+    credentials: true, // allow frontend to send cookies
   })
 );
 
@@ -27,8 +29,15 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
-app.get("/", (req, res) => {
-  res.json({ message: "Server is running on Vercel" });
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-export default app;
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
